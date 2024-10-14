d4694238900dab63840d24f0ae1cffcb8bd7f9df
root <root@localhost.localdomain>
2024-10-14 19:54:03 +0000

    add jabatan route

---
+++ src/routes/jabatan.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const JabatanController = require('../controller/jabatan');
+const router = express.Router();
+
+router.post('/', JabatanController.createNewJabatan);
+
+router.get('/', JabatanController.getAllJabatan);
+
+router.get('/:id', JabatanController.getJabatanById);
+
+router.patch(':id', JabatanController.updateJabatan);
+
+router.delete(':id', JabatanController.deleteJabatan);
+
+module.exports = router;
