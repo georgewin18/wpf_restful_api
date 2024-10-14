a14df2a3ed9b35bf3eca1a829e12cb70fd58531a
root <root@localhost.localdomain>
2024-10-14 21:13:38 +0000

    add gaji controller

---
+++ src/controller/gaji.js
@@ -0,0 +1,63 @@
+const GajiModel = require('../models/gaji');
+
+const getAllGaji = async (req, res) => {
+    try {
+        const [data] = await GajiModel.getAllGaji();
+        
+        res.json({
+            message: 'GET all gaji success',
+            data: data,
+        })
+    } catch (error) {
+        res.status(500).json({
+            message: 'Server Error',
+            serverMessage: error,
+        })
+    }
+}
+
+const getGajiById = async (req, res) => {
+    const {id} = req.params;
+
+    try {
+        const [data] = await GajiModel.getGajiById(id);
+        res.json({
+            message: 'GET gaji success',
+            data: data,
+        })
+    } catch (error) {
+        res.status(500).json({
+            message: 'Server Error',
+            serverMessage: error,
+        })
+    }
+}
+
+const createNewGaji = async (req, res) => {
+    const {body} = req;
+
+    if (!karyawan_id || !bulan || !gaji_pokok || !tunjangan || !potongan || !total_gaji) {
+        res.status(400).json({
+            message: "Terdapat kesalahan pada data input"
+        })
+    }
+    
+    try {
+        await GajiModel.createNewGaji(body);
+        res.status(201).json({
+            message: 'CREATE new gaji success',
+            data: body,
+        })
+    } catch (error) {
+        res.status(500).json({
+            message: 'Server Error',
+            serverMessage: error,
+        })
+    }
+}
+
+module.exports = {
+    getAllGaji,
+    getGajiById,
+    createNewGaji,
+}
