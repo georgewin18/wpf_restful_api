517e92eb51655a70a26393e1657a7e561d3f78d3
root <root@localhost.localdomain>
2024-10-14 21:06:28 +0000

    add gaji model

---
+++ src/models/gaji.js
@@ -0,0 +1,23 @@
+const dbPool = require('../config/database');
+
+const getAllGaji = () => {
+    const SQLQuery = `SELECT * FROM gaji`;
+    return dbPool.execute(SQLQuery);
+}
+
+const getGajiById = (id) => {
+    const SQLQuery = `SELECT * FROM gaji WHERE id=${id}`;
+    return dbPool.execute(SQLQuery);
+}
+
+const createNewJabatan = (body) => {
+    const SQLQuery = `  INSERT INTO gaji(karyawan_id, bulan, gaji_pokok, tunjangan, potongan, total_gaji)
+                        VALUES ('${body.karyawan_id}','${body.bulan}','${body.gaji_pokok}','${body.tunjangan}','${body.potongan}','${body.total_gaji}')`;
+    return dbPool.execute(SQLQuery);
+}
+
+module.exports = {
+    getAllGaji,
+    getGajiById,
+    createNewJabatan,
+}
