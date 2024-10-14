f139572a5d2a8bd279b7f6031b562a4d9c1be0c6
root <root@localhost.localdomain>
2024-10-14 23:44:39 +0000

    modification for karyawan route

--- src/routes/karyawan.js
+++ src/routes/karyawan.js
@@ -8,6 +8,10 @@ router.get('/', KaryawanController.getAllKaryawan);
 
 router.get('/:id', KaryawanController.getKaryawanById);
 
+router.get('/departemen/:id', KaryawanController.getKaryawanByDepartemenId);
+
+router.get('jabatan/:id', KaryawanController.getKaryawanByJabatanId);
+
 router.patch('/:id', KaryawanController.updateKaryawan);
 
 router.delete('/:id', KaryawanController.deleteKaryawan);
