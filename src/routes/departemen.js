acf955488dc58c43c3ca4a60c73e4f6a0f72e137
root <root@localhost.localdomain>
2024-10-14 19:23:24 +0000

    add departemen route

---
+++ src/routes/departemen.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const DepartemenController = require('../controller/departemen');
+const router = express.Router();
+
+router.post('/', DepartemenController.createNewDepartemen);
+
+router.get('/', DepartemenController.getAllDepartemen);
+
+router.get('/:id', DepartemenController.getDepartemenById);
+
+router.patch(':id', DepartemenController.updateDepartemen);
+
+router.delete(':id', DepartemenController.deleteDepartemen);
+
+module.exports = router;
